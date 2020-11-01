642e5c17c57dd553cf76e3eaa170b6124c1206f1
jw <keca13@wp.pl>
2020-11-01 10:30:14 +0100

    case

---
+++ case.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+case $1 in
+piatek) echo “weekend jest juz tuz tuz”;;
+sobota) echo “juz weekend”;;
+niedziela) echo “weekend się juz powoli konczy”;;
+poniedzialek) echo ”już poniedziałek znowu trzeba isc do pracy”;;
+*) echo „srodek tygodnia trzeba ciezko pracowac”;;
+esac 
