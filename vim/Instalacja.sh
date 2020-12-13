7beacb0af8d6edf2066074692ee75cb93c9d20d9
Jacek Wieteska <theesccode@gmail.com>
2020-12-13 18:45:56 +0100

    instalacja

---
+++ vim/Instalacja.sh
@@ -0,0 +1,22 @@
+
+https://www.vim.org/download.php
+
+On Debian/Ubuntu and Mint
+$ sudo apt install ncurses-dev git
+$ git clone https://github.com/vim/vim.git	
+$ cd vim
+$ cd src/
+$ ./configure
+$ make
+$ sudo make install
+$ vim 
+
+On RHEL/CentOS and Fedora
+# yum  install  ncurses-devel git
+# git clone https://github.com/vim/vim.git	
+# cd vim
+# cd src/
+# ./configure
+# make
+# sudo make install
+# vim
