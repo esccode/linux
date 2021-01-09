f7270e82de38c44b5938545995bef881d1bdddf8
Jacek Wieteska <theesccode@gmail.com>
2021-01-09 22:24:47 +0100

    admin

--- usefull/admin.sh
+++ usefull/admin.sh
@@ -8,19 +8,49 @@ function show_menu() {
     echo "1. Operating system Info"
     echo "2. Hostname"
     echo "3. Disk usage"
-    echo "4. Exit"
+    echo "4. Process"
+    echo "5. Exit"
 }
 
+function process_info(){
+    clear    
+    echo "1. Show all process"
+    echo "2. kill a process"
+    read -p "Enter your choice [ 1 -2 ] " choice
+        case $choice in
+        1) show_all_process ;;
+        2) kill_process ;;
+        *)
+            echo "Please enter number [ 1 - 2] "
+    esac
+}
+
+function show_all_process () {
+    clear
+    echo "Show all process"
+    ps -ef | more
+}
+
+function kill_process () {
+    clear
+    echo  "Please enter PID of the process you would like to kill:"
+    read pidkill
+    kill -9 $pidkill && echo "Process terminated successfully" || echo "Process failed to terminate"
+}
+
+
+
 function read_input(){
- read -p "Enter your choice [ 1 - 4 ] " choice   
- case $choice in
- 1) system_info ;;
- 2) host_info ;;
- 3) disk_info ;;
- 4) exit 0 ;;
- *)
-        echo "Please select number [ 1 - 4 ]"
- esac
+    read -p "Enter your choice [ 1 - 4 ] " choice   
+    case $choice in
+        1) system_info ;;
+        2) host_info ;;
+        3) disk_info ;;
+        4) process_info ;;
+        5) exit 0 ;;
+        *)
+            echo "Please select number [ 1 - 4 ]"
+    esac
 }
 
 function system_info(){
